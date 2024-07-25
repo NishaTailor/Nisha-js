5ed92827b7091a38844b6518e502af6857779fa5
Nisha Tailor <tailornisha26@gmail.com>
2024-07-25 16:25:53 +0530

    Nisha-js

---
+++ Nisha-js/exam4/index.js
@@ -0,0 +1,65 @@
+let students = []
+let totalstudents = 0
+let totalfees = 0
+
+const uimaker = () => {
+    document.getElementById('tbody').innerHTML = ''
+    totalstudents = students.length
+    document.getElementById('totalStudents').innerHTML = `Total Students: ${totalstudents}`
+    document.getElementById('totalFees').innerHTML = `Total Fees: ${totalfees}`
+
+    for (let i = 0; i < students.length; i++) {
+        let tr = document.createElement('tr')
+        let td1 = document.createElement('td')
+        td1.innerHTML = students[i].course
+        let td2 = document.createElement('td')
+        td2.innerHTML = students[i].name
+        let td3 = document.createElement('td')
+        td3.innerHTML = students[i].fee
+        let td4 = document.createElement('td')
+        td3.innerHTML = students[i].fee
+        let td5 = document.createElement('td')
+        td4.innerHTML = 'remove'
+        td4.addEventListener('click', () => removeStudent(i))
+        tr.append(td1, td2, td3, td4,td5)
+        document.getElementById('tbody').append(tr)
+    }
+}
+
+const removeStudent = (index) => {
+    let fee = students[index].fee
+    students.splice(index, 1)
+    totalfees -= fee
+    uimaker()
+}
+
+const handleData = (e) => {
+    e.preventDefault()
+
+    let student = {
+        feeAmount :document.getElementById('feeAmount').value,
+        name: document.getElementById('name').value,
+        GRID: document.getElementById('GRID').value,
+        course :document.getElementById('course').value,
+        number: document.getElementById('number').value
+    }
+
+    if (student.name.length <= 2) {
+        alert('Enter a meaningful name')
+        return
+    }
+    if (student.GRID.length !== 4) {
+        alert('Invalid GRID id')
+        return
+    }
+    if (student.number.length !== 10) {
+        alert('Invalid number')
+        return
+    }
+
+    students.push(student)
+    totalfees += feeAmount
+    uimaker()
+}
+
+document.getElementById('studentData').addEventListener('submit', handleData)
